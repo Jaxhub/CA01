cd253e81096801d0299fecef8cf0626980609af5
JGenius <55930855+Jaxhub@users.noreply.github.com>
2023-06-15 17:01:06 -0400

    complete
    This assignment is finished

--- gpt.py
+++ gpt.py
@@ -5,7 +5,7 @@ class GPT():
     ''' make queries to gpt from a given API '''
     def __init__(self,apikey):
         ''' store the apikey in an instance variable '''
-        self.apikey=apikey
+        self.apikey = apikey
         # Set up the OpenAI API client
         openai.api_key = apikey #os.environ.get('APIKEY')
 
@@ -26,6 +26,27 @@ class GPT():
         response = completion.choices[0].text
         return response
 
+    def getNewProject(self, prompt):
+        ''' Generate a GPT response to give you a new project to create '''
+        prompt = "What are some simple " + prompt + " projects I can create to get better at making " + prompt + ":\n\n"
+        completion = openai.Completion.create(
+        engine=self.model_engine,
+        prompt=prompt,
+        max_tokens=25,
+        n=3,
+        stop="\n",
+        temperature=0.8,
+    )
+
+        responses = completion.choices
+        formatted_responses = []
+        for response in responses:
+            formatted_response = "<p>" + response.text.strip() + "</p>"
+            formatted_responses.append(formatted_response)
+
+        return "\n".join(formatted_responses)
+
+    
 if __name__=='__main__':
     '''
     '''
